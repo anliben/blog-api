135f7bfe259ede8995557d52ebabdda7b33573bd
anliben <anliben.yayoi@gmail.com>
2022-09-23 23:31:44 -0300

    [feature]: added cors

--- app/main.py
+++ app/main.py
@@ -3,10 +3,25 @@ from fastapi import Depends, FastAPI
 from .dependencies import get_query_token, get_token_header
 from .internal import admin
 from .routers import noticia
+from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI(dependencies=[Depends(get_query_token)])
+# app = FastAPI(dependencies=[Depends(get_query_token)])
+app = FastAPI()
 
 
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:4200",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(noticia.router)
 app.include_router(
